8f6de31d66296618f6cf666d0e4597fb27a7cc99
Matthew Schiller <mschiller@codingdojo.com>
2020-06-22 17:29:32 -0700

    added super cool secret message

--- js/script.js
+++ js/script.js
@@ -2,4 +2,8 @@ $(document).ready(function () {
   $('.alert').click(function () {
     alert("Projects and algos 2020");
   })
+
+  $('.alert').click(function () {
+    console.log("if you're reading this, it's too late.");
+  })
 })
